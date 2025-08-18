a50cc5f1ca7b88fc7138b742c564b90a1ac2bd58
Gwang <114713193+gwang08@users.noreply.github.com>
2025-08-18 23:16:10 +0700

    add pdf file

--- modules/about/components/Resume.tsx
+++ modules/about/components/Resume.tsx
@@ -5,14 +5,15 @@ import { LuDownload as DownloadIcon } from "react-icons/lu";
 const Resume = () => {
   const t = useTranslations("AboutPage");
 
-  const RESUME_URL = "https://bit.ly/cv-satriabahari";
+  // Updated to use local PDF file
+  const RESUME_URL = "/LeMinhQuang-CV-Frontend.pdf";
 
   return (
     <Link
       href={RESUME_URL}
       target="_blank"
-      passHref
-      className="group flex w-fit items-center gap-2 rounded-lg border border-neutral-400 bg-neutral-100  px-3 py-2 text-sm transition duration-100 hover:text-neutral-800 dark:border-neutral-700 dark:bg-neutral-900 dark:hover:text-neutral-200"
+      download="LeMinhQuang-CV-Frontend.pdf"
+      className="group flex w-fit items-center gap-2 rounded-lg border border-neutral-400 bg-neutral-100 px-3 py-2 text-sm transition duration-100 hover:text-neutral-800 dark:border-neutral-700 dark:bg-neutral-900 dark:hover:text-neutral-200"
     >
       <DownloadIcon />
       <span>{t("resume_download_button")}</span>
